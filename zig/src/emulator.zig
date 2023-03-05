63e5fc9923743f05393490450c4b952497cafdd5
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-03-05 15:39:37 +0100

    Some work on rendering the display

--- zig/src/emulator.zig
+++ zig/src/emulator.zig
@@ -1,4 +1,48 @@
-const emulator = struct {
+const sdl = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
 
+const Display = @import("display.zig").Display;
+const Keyboard = @import("keyboard.zig").Keyboard;
+
+pub const Emulator = struct {
+    display: Display,
+    keyboard: Keyboard,
+
+    const Self = @This();
+
+    pub fn init() Self {
+        return Emulator{
+            .display = Display.init(),
+            .keyboard = Keyboard.init(),
+        };
+    }
+
+    pub fn run(self: *Self) void {
+        
+        mainloop: while (true) {
+            self.keyboard.handleInput();
+            self.display.draw();
+
+            var sdl_event: sdl.SDL_Event = undefined;
+            while (sdl.SDL_PollEvent(&sdl_event) != 0) {
+                switch (sdl_event.type) {
+                    sdl.SDL_KEYDOWN => {
+                        if (sdl_event.key.keysym.sym == sdl.SDLK_ESCAPE) {
+                            break :mainloop;
+                        } else { 
+                            self.keyboard.handleKeyDown(sdl_event.key.keysym.sym);
+                        }
+                    },
+                    sdl.SDL_KEYUP => self.keyboard.handleKeyUp(sdl_event.key.keysym.sym),
+                    sdl.SDL_QUIT => break :mainloop,
+                    else => {},
+                }
+            }
+        }
+
+        self.display.cleanUp();
+        sdl.SDL_Quit();       
+    }
 };
  
