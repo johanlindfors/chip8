c05cecc12978ed1b67a5418545dd0fa74da414a0
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-03-08 08:53:29 +0100

    Minor fixes after merges

--- zig/build.zig
+++ zig/build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("hello-gamedev", "src/main.zig");
+    const exe = b.addExecutable("chip8", "src/main.zig");
     exe.setBuildMode(mode);
 
     exe.addIncludePath("/usr/local/include/SDL2");
