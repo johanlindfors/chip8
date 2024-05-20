77e253de12edcab19a72df53abd06a8088b62242
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-20 10:21:20 +0200

    Refactored the interfaces

--- csharp/src/Emulator/IKeyboard.cs
+++ csharp/src/Emulator/IKeyboard.cs
@@ -1,9 +1,35 @@
+// Copyright (c) Coderox AB. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 namespace Chip8;
 
-public interface IKeyboard {
+/// <summary>
+/// The interface for a keyboard implementation.
+/// </summary>
+public interface IKeyboard
+{
+    /// <summary>
+    /// Event triggered on key pressed.
+    /// </summary>
+    /// <param name="keyCode">The key pressed.</param>
     void OnKeyPressed(int keyCode);
+
+    /// <summary>
+    /// Event triggered on key released.
+    /// </summary>
+    /// <param name="keyCode">The key released.</param>
     void OnKeyReleased(int keyCode);
 
-    bool IsPressed(int index);
+    /// <summary>
+    /// Returns if a key is pressed.
+    /// </summary>
+    /// <param name="keyCode">The key to check if pressed.</param>
+    /// <returns>If the key is pressed.</returns>
+    bool IsPressed(int keyCode);
+
+    /// <summary>
+    /// Returns all the keys.
+    /// </summary>
+    /// <returns>The boolean values of all the keys.</returns>
     bool[] GetKeys();
 }
