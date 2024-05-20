0d01e2242fb00b8ad2f213f67b9950626388890f
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-20 10:53:12 +0200

    Register module refactored

--- csharp/src/Emulator/Register.cs
+++ csharp/src/Emulator/Register.cs
@@ -1,24 +1,50 @@
+// Copyright (c) Coderox AB. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 namespace Chip8;
 
+/// <summary>
+/// Implementation of the registers.
+/// </summary>
 public class Register
 {
-    private byte[] V = new byte[16];
+    private readonly byte[] v = new byte[16];
 
-    public Register() {
-        for (int i = 0; i < 16; i++) {
-            V[i] = 0x00;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Register"/> class.
+    /// </summary>
+    public Register()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            this.v[i] = 0x00;
         }
     }
 
-    public byte Get(int index) {
-        return V[index];
+    /// <summary>
+    /// Gets the register of index.
+    /// </summary>
+    /// <param name="index">The register.</param>
+    /// <returns>The data.</returns>
+    public byte Get(int index) => this.v[index];
+
+    /// <summary>
+    /// Sets the register.
+    /// </summary>
+    /// <param name="index">The register.</param>
+    /// <param name="value">The data.</param>
+    public void Set(int index, byte value)
+    {
+        this.v[index] = value;
     }
 
-    public void Set(int index, byte value) {
-        V[index] = value;
-    }
-
-    public void Apply(int index, Func<byte,byte> func) {
-        V[index] = func(V[index]);
+    /// <summary>
+    /// Applies a function to a register.
+    /// </summary>
+    /// <param name="index">The register.</param>
+    /// <param name="func">The function.</param>
+    public void Apply(int index, Func<byte, byte> func)
+    {
+        this.v[index] = func(this.v[index]);
     }
 }
