3c3f177cd9cd7c2439a84a0125daa9bd8d4c942b
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-21 07:28:05 +0200

    All code refactored

--- csharp/src/Emulator.XNA/Main.cs
+++ csharp/src/Emulator.XNA/Main.cs
@@ -1,19 +1,35 @@
+// Copyright (c) Coderox AB. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 namespace Chip8;
 
-public class Program 
+/// <summary>
+/// The program.
+/// </summary>
+public class Program
 {
-    public static int Main(string[] args) {
-
-        if(args.Length == 0) {
+    /// <summary>
+    /// The main starting point of the application.
+    /// </summary>
+    /// <param name="args">Any eventual arguments.</param>
+    /// <returns>Error code.</returns>
+    public static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
             System.Console.WriteLine("You must supply a game argument, example: /roms/INVADERS.ch8");
             return 1;
-        } else {
+        }
+        else
+        {
             return Utils.Load(args[0])
-                .Map(bytes => {
+                .Map(bytes =>
+                {
                     var emulator = new Chip8.Emulator(bytes);
                     emulator.Run();
-                    return 0; })
-                .Reduce( () => 1);
+                    return 0;
+                })
+                .Reduce(() => 1);
         }
     }
 }
