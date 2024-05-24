37ec36bfca1ef199e6e7238d2d5bd54dbe856ee8
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-24 13:31:50 +0200

    Rendering a circle

--- java/src/main/java/se/programmeramera/chip8/App.java
+++ java/src/main/java/se/programmeramera/chip8/App.java
@@ -1,13 +1,26 @@
 package se.programmeramera.chip8;
 
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Canvas
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JFrame frame = new JFrame("My Drawing");
+        Canvas canvas = new App();
+        canvas.setSize(400, 400);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+        g.fillOval(100, 100, 200, 200);
     }
 }
