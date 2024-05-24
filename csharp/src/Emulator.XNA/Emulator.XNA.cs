ca8a2e2208d824a4a5ea3f17d692ffa1498a884d
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-24 13:22:31 +0200

    Fixes for csharp

--- csharp/src/Emulator.XNA/Emulator.XNA.cs
+++ csharp/src/Emulator.XNA/Emulator.XNA.cs
@@ -5,7 +5,6 @@ namespace Chip8;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System.Xml.Schema;
 using Input = Microsoft.Xna.Framework.Input;
 
 /// <summary>
@@ -59,7 +58,8 @@ public class Emulator : Game
 
         var random = new RandomNumberGenerator();
         var registers = new Registers();
-        this.cpu = new CPU(this.memory, registers, random, this.keyboard, screen);
+        var audio = new AudioPlayer();
+        this.cpu = new CPU(this.memory, registers, random, this.keyboard, screen, audio);
 
         this.GraphicsDevice.SamplerStates[0] = SamplerState.PointWrap;
 
