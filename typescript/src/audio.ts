8b13b5fedc7c118059a497458c2de5ef22feed61
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-26 10:52:07 +0200

    Audio in typescript

--- typescript/src/audio.ts
+++ typescript/src/audio.ts
@@ -5,15 +5,28 @@ interface IAudio {
 }
 
 class AudioPlayer implements IAudio {
+    private context : AudioContext;
+    private oscillator : OscillatorNode;
+    private isSoundPlaying : boolean = false;
+
     isPlaying(): boolean {
-        return false;
+        return this.isSoundPlaying;
     }
 
     start() : void {
-
+        this.context = new AudioContext()
+        this.oscillator = this.context.createOscillator()
+        this.oscillator.type = "sine"
+        this.oscillator.connect(this.context.destination)
+        this.oscillator.start()
+        this.isSoundPlaying = true;
     }
 
     stop() : void {
+        if(this.isSoundPlaying === false) return;
         
+        this.isSoundPlaying = false;
+        this.oscillator.stop();
+        this.context.close();
     }
 }
