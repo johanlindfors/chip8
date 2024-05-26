07adae5b19b50d93c30cb7c6722c88c92a0c8ad6
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-26 10:38:13 +0200

    Removed the api

--- typescript/src/api.ts
+++
@@ -1,8 +0,0 @@
-class ApiClient {
-    async getRom(filename: string) : Promise<ArrayBuffer> {
-        let rom = await window.fetch(filename)
-            .then(response => response.arrayBuffer());
-        return rom;
-    }
-
-}
