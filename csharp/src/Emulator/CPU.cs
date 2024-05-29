f4391482ff9d4c6bffe682cd64035db026dd79f1
Johan Lindfors <johan.lindfors@coderox.se>
2024-05-29 13:14:16 +0200

    Fixes

--- csharp/src/Emulator/CPU.cs
+++ csharp/src/Emulator/CPU.cs
@@ -100,7 +100,7 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
     public int EmulateCycle()
     {
         this.InstructionsCounter++;
-        int opcode = this.memory.GetOpcode(this.pc);
+        int opcode = this.memory.GetOpcode(this.pc) & 0xFFFF;
 
         int x = (opcode & 0x0F00) >> 8;
         int y = (opcode & 0x00F0) >> 4;
@@ -113,35 +113,43 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
 
         this.pc += 2;
 
-        switch (opcode)
-        {
-            case 0x00E0: // Clears the screen
-                this.screen.Clear();
-                this.screen.SetDrawFlag();
-                return 109;
-
-            case 0x000E:
-                this.pc = this.i;
-                return 1;
-
-            case 0x00EE: // Returns from a subroutine.
-                this.pc = this.stack.Pop();
-                return 105;
-
-            default:
-                break;
-        }
-
         switch (opcode & 0xF000)
         {
+            case 0x0000:
+                switch (opcode)
+                {
+                    case 0x00E0: // Clears the screen
+                        this.screen.Clear();
+                        this.screen.SetDrawFlag();
+                        clockCycles = 109;
+                        break;
+
+                    case 0x000E:
+                        this.pc = this.i;
+                        clockCycles = 1;
+                        break;
+
+                    case 0x00EE: // Returns from a subroutine.
+                        this.pc = this.stack.Pop();
+                        clockCycles = 105;
+                        break;
+
+                    default:
+                        throw new NotSupportedException($"OpCode '{opcode:X4}' is not supported!");
+                }
+
+                break;
+
             case 0x1000: // Jumps to address nnn.
                 this.pc = nnn;
-                return 105;
+                clockCycles = 105;
+                break;
 
             case 0x2000: // Calls subroutine at nnn.
                 this.stack.Push(this.pc);
                 this.pc = nnn;
-                return 105;
+                clockCycles = 105;
+                break;
 
             case 0x3000: // Skips the next instruction if vx equals nn. (Usually the next instruction is a jump to skip a code block)
                 clockCycles = 55;
@@ -154,7 +162,8 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
                     clockCycles += 9;
                 }
 
-                return clockCycles;
+                break;
+
             case 0x4000: // Skips the next instruction if vx doesn't equal nn. (Usually the next instruction is a jump to skip a code block)
                 clockCycles = 55;
                 if (vx != nn)
@@ -166,7 +175,7 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
                     clockCycles += 9;
                 }
 
-                return clockCycles;
+                break;
 
             case 0x5000: // Skips the next instruction if vx equals vy. (Usually the next instruction is a jump to skip a code block)
                 clockCycles = 55;
@@ -179,35 +188,38 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
                     clockCycles += 9;
                 }
 
-                return clockCycles;
+                break;
 
             case 0x6000: // Sets vx to nn.
                 this.registers[x] = nn;
-                return 27;
+                clockCycles = 27;
+                break;
 
             case 0x7000: // Adds nn to vx. (Carry flag is not changed)
                 this.registers.Apply(x, vx => (byte)((vx + nn) & 0xFF));
-                return 45;
+                clockCycles = 45;
+                break;
 
             case 0x8000:
                 {
+                clockCycles = 200;
                 switch (n)
                 {
                     case 0x0000: // Sets vx to the value of vy.
                         this.registers[x] = vy;
-                        return 200;
+                        break;
 
                     case 0x0001: // Sets vx to vx or vy. (Bitwise OR operation)
                         this.registers.Apply(x, vx => (byte)(vx | vy));
-                        return 200;
+                        break;
 
                     case 0x0002: // Sets vx to vx and vy. (Bitwise AND operation)
                         this.registers.Apply(x, vx => (byte)(vx & vy));
-                        return 200;
+                        break;
 
                     case 0x0003: // Sets vx to vx xor vy.
                         this.registers.Apply(x, vx => (byte)(vx ^ vy));
-                        return 200;
+                        break;
 
                     case 0x0004: // Adds vy to vx. VF is set to 1 when there's a carry, and to 0 when there isn't.
                         this.registers.Apply(x, vx =>
@@ -216,29 +228,29 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
                             this.registers[0xF] = (byte)(sum > 0xFF ? 1 : 0);
                             return (byte)(sum & 0xFF);
                         });
-                        return 200;
+                        break;
 
                     case 0x0005: // vy is subtracted from vx. VF is set to 0 when there's a borrow, and 1 when there isn't.
                         this.registers[0xF] = (byte)(vy > vx ? 0 : 1);
                         this.registers.Apply(x, vx => (byte)(vx - vy));
-                        return 200;
+                        break;
 
                     case 0x0006: // Stores the least significant bit of vx in VF and then shifts vx to the right by 1.[2]
                         this.registers[0xF] = (byte)(vx & 0x1);
                         this.registers.Apply(x, vx => (byte)(vx >> 1));
-                        return 200;
+                        break;
 
                     case 0x0007: // Sets vx to vy minus vx. VF is set to 0 when there's a borrow, and 1 when there isn't.
                         this.registers[0xF] = (byte)(vx > vy ? 0 : 1);
                         this.registers.Apply(x, vx => (byte)((vy - vx) & 0xFF));
-                        return 200;
+                        break;
 
                     case 0x000E: // Stores the most significant bit of vx in VF and then shifts vx to the left by 1.[3]
                         this.registers[0xF] = (byte)((vx & 0x80) > 0 ? 1 : 0);
                         this.registers.Apply(x, vx => (byte)((vx << 1) & 0xFF));
-                        return 200;
-                    default:
                         break;
+                    default:
+                        throw new NotSupportedException($"OpCode '{opcode:X4}' is not supported!");
                     }
                 }
 
@@ -255,36 +267,35 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
                     clockCycles += 9;
                 }
 
-                return clockCycles;
+                break;
 
             case 0xA000: // Sets I to the address nnn.
                 this.i = nnn;
-                return 55;
+                clockCycles = 55;
+                break;
 
             case 0xB000: // Jumps to the address nnn plus V0..
                 this.pc = (this.registers[0] + nnn) & 0x0FFF;
-                return 105;
+                clockCycles = 105;
+                break;
 
             case 0xC000: // Sets vx to the result of a bitwise and operation on a random number (Typically: 0 to 255) and nn.
                 this.registers[x] = (byte)(this.random.NextByte() & nn);
-                return 164;
+                clockCycles = 164;
+                break;
 
             case 0xD000: // Draws a sprite at coordinate (vx, vy) that has a width of 8 pixels and a height of n pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
                 int height = n;
-
                 this.registers[0xF] = 0;
-
                 for (int yLine = 0; yLine < height; yLine++)
                 {
                     int pixelValue = this.memory.GetByte(this.i + yLine);
-
                     for (int xLine = 0; xLine < 8; xLine++)
                     {
                         if (Utils.GetBitValue(pixelValue, xLine) != 0)
                         {
                             int xCoord = vx + xLine;
                             int yCoord = vy + yLine;
-
                             if (xCoord >= this.screen.Width)
                             {
                                 xCoord %= this.screen.Width;
@@ -306,94 +317,109 @@ public class CPU(Memory memory, Registers registers, IRandomNumberGenerator rand
                 }
 
                 this.screen.SetDrawFlag();
-                return 22734;
+                clockCycles = 22734;
+                break;
 
             case 0xE000:
+                clockCycles = 73;
                 switch (nn)
                 {
                     case 0x9E: // Skips the next instruction if the key stored in vx is pressed. (Usually the next instruction is a jump to skip a code block)
                         this.pc += this.keyboard.IsPressed(vx) ? 2 : 0;
-                        return 73;
+                        break;
 
                     case 0xA1: // Skips the next instruction if the key stored in vx isn't pressed. (Usually the next instruction is a jump to skip a code block)
                         this.pc += this.keyboard.IsPressed(vx) ? 0 : 2;
-                        return 73;
+                        break;
 
                     default:
-                        break;
+                        throw new NotSupportedException($"OpCode '{opcode:X4}' is not supported!");
                 }
 
                 break;
 
             case 0xF000:
 
-                switch (nn)
+                switch (opcode & 0x00FF)
                 {
-                    case 0x07: // Sets vx to the value of the delay timer.
+                    case 0x0007: // Sets vx to the value of the delay timer.
                         this.registers[x] = (byte)(this.delayTimer & 0xFF);
-                        return 45;
+                        clockCycles = 45;
+                        break;
 
-                    case 0x0A: // A key press is awaited, and then stored in vx. (Blocking Operation. All instruction halted until next key event)
+                    case 0x000A: // A key press is awaited, and then stored in vx. (Blocking Operation. All instruction halted until next key event)
                         for (int i = 0; i < this.keyboard.GetKeys().Length; i++)
                         {
                             if (this.keyboard.IsPressed(i))
                             {
                                 this.registers[x] = (byte)i;
-                                return 1;
+                                clockCycles = 1;
                             }
                         }
 
                         this.pc -= 2;
-                        return 1;
+                        clockCycles = 1;
+                        break;
 
-                    case 0x15: // Sets the delay timer to vx.
+                    case 0x0015: // Sets the delay timer to vx.
                         this.delayTimer = (byte)(vx & 0xFF);
-                        return 45;
+                        clockCycles = 45;
+                        break;
 
-                    case 0x18: // Sets the sound timer to vx.
+                    case 0x0018: // Sets the sound timer to vx.
                         this.soundTimer = (byte)(vx & 0xFF);
-                        return 45;
+                        clockCycles = 45;
+                        break;
 
-                    case 0x1E: // Adds vx to I.
+                    case 0x001E: // Adds vx to I.
                         this.i += vx & 0xFF;
-                        return 86;
+                        clockCycles = 86;
+                        break;
 
-                    case 0x29: // Sets I to the location of the sprite for the character in vx. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+                    case 0x009: // Sets I to the location of the sprite for the character in vx. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
                         this.i = 0x50 + (vx * 5);
                         return 91;
 
-                    case 0x33: // vx, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of vx, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
+                    case 0x0033: // vx, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of vx, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.)
                         this.memory.SetByte(this.i, (byte)(vx / 100));
                         this.memory.SetByte(this.i + 1, (byte)((vx % 100) / 10));
                         this.memory.SetByte(this.i + 2, (byte)(vx % 10));
-                        return 927;
+                        clockCycles = 927;
+                        break;
 
-                    case 0x55: // Stores V0 to vx (including vx) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
+                    case 0x0055: // Stores V0 to vx (including vx) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
                         for (int p = 0; p <= x; p++)
                         {
                             this.memory.SetByte(this.i + p, (byte)this.registers[p]);
                         }
 
-                        return 605 + (x * 64);
+                        clockCycles = 605 + (x * 64);
+                        break;
 
-                    case 0x65: // Fills V0 to vx (including vx) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
+                    case 0x0065: // Fills V0 to vx (including vx) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
                         for (int p = 0; p <= x; p++)
                         {
                             this.registers[p] = (byte)(this.memory.GetByte(this.i + p) & 0xFF);
                         }
 
-                        return 605 + (x * 64);
+                        clockCycles = 605 + (x * 64);
+                        break;
 
                     default:
-                        break;
+                        throw new NotSupportedException($"OpCode '{opcode:X4}' is not supported!");
                     }
 
                 break;
 
             default:
+                if (clockCycles == 0)
+                {
+                    throw new NotSupportedException($"OpCode '{opcode:X4}' is not supported!");
+                }
+
                 break;
         }
 
-        return 0;
+        return clockCycles;
     }
 }
