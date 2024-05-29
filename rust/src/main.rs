6df0a529eeb0f4a5aad53d6973a273e7eb1f09ff
Johan Lindfors <Johan.Lindfors@coderox.se>
2024-05-29 19:06:10 +0200

    Rust now able to launch in vs code with rom

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -11,6 +11,10 @@ pub mod implementations;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
     let file_path = &args[1];
     let data = fs::read(file_path).expect("Failed to open file");
 
